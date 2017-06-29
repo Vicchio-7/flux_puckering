ee0166dd74a39b357e295e38c4e31af63d818be2
Vicchio-7 <vicchio@umich.edu>
2017-06-29 15:05:26 -0400

    sending

--- z11_freeze_jobs.sh
+++ z11_freeze_jobs.sh
@@ -115,7 +115,7 @@ elif [ ${status_build} == 0 ] ; then
             sed -i "s/\$num_procs/${cores_per_node}/g" temp1.temp
             sed -i "s/\$folder_1/${folder}/g" temp1.temp
             sed -i "s/\$folder_new/${molecule_type}-freeze_${level_short}/g"  temp1.temp
-            sed -i "s/\$chkfile/${molecule_type}-${file}-freeze_${level_short}.chk/g"  temp1.temp
+            sed -i "s/\$chkfile/${file}-freeze_${level_short}.chk/g"  temp1.temp
             sed -i "s/\level_of_theory/${level_theory}/g" temp1.temp
 
             mv temp1.temp ${file}.com
