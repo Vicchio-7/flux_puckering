0cb210052e5a57597c2c7369a32a77d6fd0e0693
Vicchio-7 <vicchio@umich.edu>
2017-07-03 13:24:46 -0400

    sending

--- z15_irc_run.sh
+++ z15_irc_run.sh
@@ -18,8 +18,8 @@ level_short=$3
 
 ## Input - Gaussian Run Information ##
 # The following information determines the numbers of cores and memory the jobs will require.
-cores_per_node=2
-memory_job=4000
+cores_per_node=1
+memory_job=3800
 hours=12 #1, 2 ,3 ..... 10, 11, 12....
 minutes=00 # number between 0 and 59
 
