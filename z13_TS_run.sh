04b8f6f281e54331ae3dfb6ea5814203da6ea415
Vicchio-7 <vicchio@umich.edu>
2017-07-03 10:42:34 -0400

    sending

--- z13_TS_run.sh
+++ z13_TS_run.sh
@@ -199,19 +199,21 @@ elif [ ${status_build} == 2 ] ; then
         else
                 tpl_file=${tpl}/${tpl_folder}/TS_levo_from_checkpoint.tpl
 
-            ######## The section below updates the Gaussian Input File
+            echo 'wait'
 
-                sed -e "s/\$memory/${total_memory}/g" ${tpl_file} > temp1.temp
-                sed -e "s/\$num_procs/${cores_per_node}/g" temp1.temp >> temp2.temp
-                sed -e "s/\$folder_1/${folder}/g" temp2.temp >> temp3.temp
-                sed -e "s/\$folder_old/${molecule_type}-freeze_${level_short}/g" temp3.temp >> temp4.temp
-                sed -e "s/\$old_check/${file}-freeze_${level_short}.chk/g" temp4.temp >> temp5.temp
-                sed -e "s/\$folder_new/${molecule_type}-TS_${level_short}/g" temp5.temp >> temp6.temp
-                sed -e "s/\$chkfile/${file}-freeze_${level_short}-${job_type}_${level_short}.chk/g" temp6.temp >> temp7.temp
-                sed -e "s/\level_of_theory/${level_theory}/g" temp7.temp >> temp8.temp
-
-                mv temp8.temp ${file}.com
-                rm *.temp
+#            ######## The section below updates the Gaussian Input File
+#
+#                sed -e "s/\$memory/${total_memory}/g" ${tpl_file} > temp1.temp
+#                sed -e "s/\$num_procs/${cores_per_node}/g" temp1.temp >> temp2.temp
+#                sed -e "s/\$folder_1/${folder}/g" temp2.temp >> temp3.temp
+#                sed -e "s/\$folder_old/${molecule_type}-freeze_${level_short}/g" temp3.temp >> temp4.temp
+#                sed -e "s/\$old_check/${file}-freeze_${level_short}.chk/g" temp4.temp >> temp5.temp
+#                sed -e "s/\$folder_new/${molecule_type}-TS_${level_short}/g" temp5.temp >> temp6.temp
+#                sed -e "s/\$chkfile/${file}-freeze_${level_short}-${job_type}_${level_short}.chk/g" temp6.temp >> temp7.temp
+#                sed -e "s/\level_of_theory/${level_theory}/g" temp7.temp >> temp8.temp
+#
+#                mv temp8.temp ${file}.com
+#                rm *.temp
         fi
 
         ######## The section below creates the PBS file for submission on Flux
