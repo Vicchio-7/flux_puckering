ed5ecb4b7cbcb1f88774a9093c78066036699eb5
Vicchio-7 <vicchio@umich.edu>
2017-07-03 09:27:30 -0400

    sending

--- z01_create-directory.sh
+++ z01_create-directory.sh
@@ -45,6 +45,10 @@ elif [ "${molecule_type}" == 'aglc' ] ;  then
 	folder=4_alphagluc
 	status_build=0
 	ext=.xyz
+elif [ "${molecule_type}" == 'levo' ] ;  then
+	folder=7_levo
+	status_build=0
+	ext=.xyz
 else
 	echo
 	echo "The molecule type is not found in this script"
