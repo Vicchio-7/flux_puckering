ab5eca81ce5d44245b925ac0b1c4199198196e47
Vicchio-7 <vicchio@umich.edu>
2017-07-03 11:30:42 -0400

    sending

--- z13_TS_run.sh
+++ z13_TS_run.sh
@@ -209,7 +209,7 @@ elif [ ${status_build} == 2 ] ; then
 
                 tail -n 24 ../0_initial-coordinates/${file_unedit} >> temp1.temp
 
-                echo '' >> temp1.temp
+                echo 'hi mom' >> temp1.temp
 
                 mv temp1.temp ${file}.com
 
