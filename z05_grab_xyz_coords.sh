0e2c3067b2b5e548c99ba8330165f33acad6be4e
Vicchio-7 <vicchio@umich.edu>
2017-07-06 10:10:55 -0400

    sending

--- z05_grab_xyz_coords.sh
+++ z05_grab_xyz_coords.sh
@@ -14,28 +14,36 @@
 
 molecule_type=$1
 
+
+main=/home/${user}/1_puckering
+
 # --------------------------------------------------------------------------------------
 
 ## Setup Check ##
 
 if [ "${molecule_type}" == 'oxane' ] ; then
 	status_build=0
+	folder=1_oxane
 	num_atoms=16
 	buffer_lines=4
 elif [ "${molecule_type}" == 'bxyl' ] ;  then
 	status_build=0
+	folder=2_bxyl
 	num_atoms=20
 	buffer_lines=4
 elif [ "${molecule_type}" == 'bglc' ] ;  then
 	status_build=0
+	folder=3_betagluc
 	num_atoms=24
 	buffer_lines=4
 elif [ "${molecule_type}" == 'levo' ] ;  then
-	status_build=0
+	status_build=2
+	folder=7_levo
 	num_atoms=24
 	buffer_lines=4
 elif [ "${molecule_type}" == 'dehy' ] ;  then
-	status_build=0
+	status_build=2
+	folder=8_dehy
 	num_atoms=24
 	buffer_lines=4
 else
@@ -62,6 +70,25 @@ elif [ ${status_build} == 0 ] ; then
         	echo "     ${num_atoms}" > $filename
         	echo $log >> $filename
         	grep -A $lines_grab 'Input orientation:' $log | tail -${num_atoms} | awk '{print "  " $2 "  " $4 "  " $5 "  " $6}' >> $filename
+	done
+
+elif [ ${status_build} == 2 ] ; then
+
+
+    if [ ! -d ${main}/z_results/${folder}/coordinates/ ]; then
+        mkdir ${main}/z_results/${folder}/coordinates/.
+    fi
+
+   lines_grab=$((num_atoms + buffer_lines))
+
+   	# For each file type ending in .log
+	for log in $(find . -type f -iname '*.log'); do
+        	filename="${log%.*}.xyz"
+        	echo "     ${num_atoms}" > $filename
+        	echo $log >> $filename
+        	grep -A $lines_grab 'Input orientation:' $log | tail -${num_atoms} | awk '{print "  " $2 "  " $4 "  " $5 "  " $6}' >> $filename
+
+	        cp ${filename} ${main}/z_results/${folder}/coordinates/.
 	done
 
 fi
