fb40001f9cee48aeace659119c3376eea4ba2742
Vicchio-7 <vicchio@umich.edu>
2017-07-26 10:18:20 -0400

    sending

--- z14_norm_analysis.sh
+++ z14_norm_analysis.sh
@@ -116,20 +116,19 @@ elif [ ${status_build} == 0 ] ; then
 
             mv temp1.temp ${file_org}-norm_${3}.com
 
+            ######## The section below creates the Slurm file for submission on Bridges
+            sed -e "s/\$num_proc/${cores_per_node}/g" ${tpl}/gaussian_pbs_script.job > temp1.txt
+            sed -i "s/\$memory/${total_memory}/g" temp1.txt
+            sed -i "s/conform/${file_org}-ircr/g" temp1.txt
+            sed -i "s/gauss-log/${file_org}-ircr_${3}/g" temp1.txt
+            sed -i "s/\$molecule/${molecule_type}/g" temp1.txt
+            sed -i "s/\$test/${job_type}/g" temp1.txt
+            sed -i "s/\$level/${level_short}/g" temp1.txt
+            sed -i "s/\$hours/${hours}/g" temp1.txt
+            sed -i "s/\$minutes/${minutes}/g" temp1.txt
 
-        ######## The section below creates the PBS file for submission on Bridges
+            mv temp1.txt pbs-${file_org}.job
 
-                    sed -e "s/\$num_proc/${cores_per_node}/g" ${tpl}/gaussian_pbs_script.job > temp1.txt
-                    sed -i "s/\$memory/${total_memory}/g" temp1.txt
-                    sed -i "s/conform/${file}/g" temp1.txt
-                    sed -i "s/gauss-log/${file}-freeze_${3}/g" temp1.txt
-                    sed -i "s/\$molecule/${molecule_type}/g" temp1.txt
-                    sed -i "s/\$test/${job_type}/g" temp1.txt
-                    sed -i "s/\$level/${level_short}/g" temp1.txt
-                    sed -i "s/\$hours/${hours}/g" temp1.txt
-                    sed -i "s/\$minutes/${minutes}/g" temp1.txt
-
-                    mv temp1.txt pbs-${file_org}.job
         fi
     done
 fi
