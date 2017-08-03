6d0f9b3f4195cb3f0cd8ad26ba4430f684538ede
Vicchio-7 <vicchio@umich.edu>
2017-08-03 11:10:23 -0400

    sending

--- z18_dftb_runs.sh
+++ z18_dftb_runs.sh
@@ -161,7 +161,7 @@ elif [ ${status_build} == 0 ] ; then
 
             head -n 4 ${tpl_file} >> temp1.temp
             tail -n 22 ../0_initial-coordinates/${file}.xyz >> temp1.temp
-            tail -n 34 ${tpl_file} >> temp1.temp
+            tail -n 33 ${tpl_file} >> temp1.temp
             mv temp1.temp ${file}.com
 
 #            sed -i '$d' ${file}.com
@@ -232,8 +232,10 @@ elif [ ${status_build} == 0 ] ; then
 
         ######## The section below creates the Slurm file for submission on Bridges
 
-            sed -e "s/\$num_proc/${cores_per_node}/g" ${tpl}/../gaussian_slurm_script.job > temp1.txt
+
+            sed -e "s/\$num_proc/${cores_per_node}/g" ${tpl}/gaussian_pbs_script.job > temp1.txt
             sed -i "s/conform/${file}/g" temp1.txt
+            sed -i "s/\$memory/${total_memory}/g" temp1.txt
             sed -i "s/gauss-log/${1}-${file}-freeze_${3}-optall_${3}/g" temp1.txt
             sed -i "s/\$molecule/${molecule_type}/g" temp1.txt
             sed -i "s/\$test/${job_type}/g" temp1.txt
